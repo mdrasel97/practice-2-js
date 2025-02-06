c580ade038d14279d96cce3c7a6928fcf0cd2e92
mdrasel97 <mollarasel972@gmail.com>
2025-02-06 14:00:32 +0600

    if else added

--- script.js
+++ script.js
@@ -19,7 +19,8 @@
 // let applePrice = 280;
 // console.log(orangePrice == applePrice);
 
-
+// console.log(10 == '10');
+// console.log(10 === '10');
 
 
 
@@ -43,3 +44,159 @@
 // else{
 //     console.log(price);
 // }
+
+
+// let age = 8;
+// if(age > 10){
+//     console.log('welcome to mela');
+// }
+// else{
+//     console.log('mela apnar jonno noy');
+// }
+
+// let age = 12;
+// if(age >= 18){
+//     console.log('apni melay maam niye jete parben');
+// }
+// else if(age >= 15){
+//     console.log('apni gf niye jete parber na');
+// }
+// else{
+//     console.log('apni akhono picchi');
+// }
+
+// let age = 20;
+// if(age >= 18 )
+
+
+
+
+
+
+// let age = 15;
+// if(age >= 18){
+//     console.log('tomi amar jantus');
+// }
+// else{
+//     console.log('tomi akhono pichhi')
+// }
+
+
+// if(true){
+//     let y = 20;
+// }
+// console.log(y);
+// let y = 20;
+// if(true){
+// }
+// console.log(y);
+
+// let age = 20;
+// let rasel = true;
+// if(age >= 18){
+//     console.log('apni melay jete parben');
+//     if(rasel >= true){
+//         console.log('apni vut dekte parben');
+//     }
+//     else{
+//         console.log('apni vut dekte parben na')
+//     }
+// }
+// else{
+//     console.log('apni akhono choto')
+// }
+
+// if(11 < 10){
+//     console.log('i have a smallar');
+// }
+// else{
+//     console.log('yes')
+// }
+// let biriyaniPrice = 200;
+// if(biriyaniPrice < 250){
+//     console.log('mama biriyani dao')
+// }
+// else{
+//     console.log('mama tk kom ache');
+// }
+// let whegit = 40;
+// if(whegit < 20){
+//     console.log('ami hate kore niya asbo');
+// }
+// else{
+//     console.log('ami riksha nibo');
+// }
+
+// let taka = 300;
+// if(taka > 200){
+//     console.log('sinema dekte jabo');
+// }
+// else{
+//     console.log('kaka muri dao')
+// }
+
+// let salary = 25000;
+// let bcs = false;
+// if(salary >= 20000){
+//     console.log('pola amar mone dorche');
+//     if(bcs){
+//         console.log('wow aro valo holo');
+//     }
+//     else{
+//         console.log('amar bcs lagbe');
+//     }
+// }
+// else{
+//     console.log('pola amar mon moto hoy nai')
+// }
+// let salary = 25000;
+// let bcs = true;
+// if(salary >= 20000 && bcs){
+//     console.log('pola amar mone dorche');
+// }
+// else{
+//     console.log('pola amar mon moto hoy nai')
+// }
+
+// let price = 3000;
+// if(price >= 5000){
+//     let discount = price * 10 / 100;
+//     let payamount = price - discount;
+//     console.log(payamount);
+// }
+// else if(price >= 2000){
+//     let discount = price * 5 /100;
+//     let payamount = price - discount;
+//     console.log(payamount);
+// }
+// else{
+//     console.log(price);
+// }
+
+
+// let age = 55;
+// let price = 500;
+// if(age <= 12){
+//     console.log('you child for free eat')
+// }
+// else if(age >= 50){
+//     let discount = price * 25 / 100;
+//     let payamount = price - discount;
+//     console.log(payamount);
+// }
+// else{
+//     console.log(price);
+// }
+
+
+
+let mark = 65;
+if(mark >= 80){
+    console.log('a')
+}
+else if(mark >= 70){
+    console.log('b')
+}
+else if(mark >= 60){
+    console.log('c')
+}
