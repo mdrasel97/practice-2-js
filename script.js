646904684e018dbbf2f078f9fa8d2bad96a17527
mdrasel97 <mollarasel972@gmail.com>
2025-02-06 10:43:51 +0600

    comparison added

---
+++ script.js
@@ -0,0 +1,45 @@
+// variable
+// Number
+// let age = 25;
+
+// // string 
+// let num = "md rasel";
+
+// // boolean 
+// let failed = false;
+
+// bigger= >
+// small = >
+// same ==
+// same or big = >=
+// same or small= <=
+// not same !=
+
+// let orangePrice = 220;
+// let applePrice = 280;
+// console.log(orangePrice == applePrice);
+
+
+
+
+
+// const price = 8000;
+// if(price => 5000){
+//     const discount = price * 10 / 100;
+//     console.log(discount);
+// }
+
+// let price = 7000;
+// if(price => 5000){
+//     let discount = price * 10 / 100;
+//     console.log(discount);
+// }
+
+// let price = 400;
+// if(price >= 500){
+//     let discount = price * 5 / 100;
+//     console.log(discount);
+// }
+// else{
+//     console.log(price);
+// }
