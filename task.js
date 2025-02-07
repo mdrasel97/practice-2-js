a907c0bf1c69e7383818c833c0ac084dd3a1f675
mdrasel97 <mollarasel972@gmail.com>
2025-02-07 14:40:28 +0600

    js condition if else added

---
+++ task.js
@@ -0,0 +1,142 @@
+// let burgarPrice = 400;
+// if(burgarPrice >= 500){
+//     console.log('apnar jonno akta free drinks');
+// }
+// else{
+//     console.log('please pay for cook price 30tk');
+// }
+
+
+
+// let marks = 100;
+// if(marks >= 90 && marks<= 100){
+//     console.log('A')
+// }
+// else if(marks >= 80 < 89){
+//     console.log('B')
+// }
+// else if(marks >= 60){
+//     console.log('C')
+// }
+// else if(marks >= 0){
+//     console.log('D');
+// }
+// else if(score<=89 && score>79)
+
+
+
+
+
+// bmi calculator
+// if(bmi <= 18.5){
+//     console.log('you are a underweight')
+// }
+// else if(bmi >= 18.5 && bmi <= 24.9){
+//     console.log('you are normal');
+// }
+// else if(bmi >= 25 && bmi <= 29.9){
+//     console.log('you are overweight')
+// }
+// else{
+//     console.log('you are obese')
+// }
+
+
+// nestad frinds
+// let myScore = 50;
+// let friendScore = 80;
+// if(myScore >= 80){
+//     console.log('tomi amar fried er score check koro...');
+//     if(friendScore >=80){
+//         console.log('bondhu tomi ajke amar sathe lunch korba');
+//     }
+//     else if(friendScore >= 60){
+//         console.log('good luck bondhu next time dekha hobe')
+//     }
+//     else if(friendScore >= 40 && friendScore <= 59){
+//         console.log('unseen your friend massege');
+//     }
+//     else if(friendScore < 40){
+//         console.log('bondu tomi block')
+//     }
+// }
+// else{
+//     console.log('go to home and sleep and act sad')
+// }
+
+
+// // ternary oparetor
+// let num1 = 40;
+// let num2 = 50;
+// if(num1 > num2){
+    
+//     console.log(num1 * 2);
+// }
+// else{
+//     console.log(num1 + num2);
+// }
+// num1 > num2 ? console.log(num1 * 2) : console.log(num1 + num2)
+
+
+// let num1 = 80;
+// let num2 = 50;
+// let result;
+// // if(num1 > num2){
+// //     result = num1 * 2;
+// // }
+// // else {
+// //     result = num1 + num2;
+// // }
+// // console.log(result);
+
+// result = num1 > num2 ? num1 * 2 : num1 + num2;
+// console.log(result)
+
+
+// bus calculator 
+// let age = 28;
+// let taka = 800;
+// if(age < 10){
+//     console.log('tomi akjon child, tomar jonno free')
+// }
+
+//     else if(age > 10 && age <=30){
+//         let student = taka * 50 /100;
+//         let payAmount = taka - student;
+//         console.log(payAmount);
+//     }
+//     else if(age > 60){
+//         let seniorSitizen = taka * 15 /100;
+//         let payAmount = taka - seniorSitizen;
+//         console.log(payAmount);
+//     }
+//     else {
+//         console.log(taka)
+//     }
+
+
+// student mark result 
+let marks = 102;
+let grade;
+if(marks >= 90 && marks <=100){
+    grade = "A"
+}
+else if(marks >= 80 && marks <= 89){
+    grade = "B"
+}
+else if(marks >= 70 && marks <= 79){
+    grade = "C"
+}
+else if(marks >= 60 && marks <= 69){
+    grade = "D"
+}
+else if(marks >= 0 && marks <= 59){
+    grade = "F"
+}
+else{
+    grade = "invalide score"
+}
+console.log('your grade', grade)
+
+
+
